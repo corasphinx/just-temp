afbb81f62cf95e9848ba25dab9be40f5779ab834
arnoldwang <arnoldprodev@gmail.com>
2022-11-05 19:03:01 +0800

    init the front end with vite-react-typescript

---
+++ src-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="border border-black text-danger">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src-frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+
+// import bootstrap
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
